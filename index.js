065512e7ae3e5522618c3b5d8be381a1c1bc6fef
phandat128 <phandat12082002@gmail.com>
2022-07-19 22:19:41 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 import Student from './Student.js'
 import Point from './Point.js'
+import fs from 'fs'
 
-var student1 = new Student(1,'Nguyen Van A', 19, 'nam', new Point(7,8.5,8,9), 'KHMT04')
+/*** create json file
+var student1 = new Student(1232,'Nguyen Van An', 18, 'nam', new Point(7,8.5,8,9), 'KHMT04')
+var student2 = new Student(3423,'Pham Thi Bich', 19, 'nu', new Point(5,6.5,8.5,4.5), 'KHMT04')
+var student3 = new Student(2345,'Hoang Van Khanh', 19, 'nam', new Point(6.25,7,9,3.5), 'KHMT04')
+var student4 = new Student(9813,'Ha Thi Hong', 19, 'nu', new Point(7,8,4.5,6), 'KHMT04')
+var student5 = new Student(7238,'Vo Van Nam', 20, 'nam', new Point(9,7.5,8.5,9.5), 'KHMT04')
 
-console.log(student1.avaragePoint())
+var studentList = [student1,student2,student3,student4,student5];
+
+var json = JSON.stringify(studentList)
+
+fs.writeFile('studentList.json', json, 'utf8', ()=> {
+    return 0
+})
+*/
+
+fs.readFile('studentList.json', 'utf8',  function(err, data){
+    if (err){
+        return console.log('error')
+    }
+    let studentList = jsonToStudent(data)
+    
+    console.log(highestPointStudent(studentList))
+    console.log(lowestPointStudent(studentList))
+})
+
+function jsonToStudent(data){
+    const list = JSON.parse(data)
+    let studentList = []
+    for(let e of list){
+        let pointObj = Object.assign(new Point(),e.point)
+        let student = Object.assign(new Student(), e)
+        student.point = pointObj
+        studentList.push(student)
+    }
+    return studentList
+}
+
+function lowestPointStudent(list){
+    list.sort((a,b) => {
+        return (a.avaragePoint() - b.avaragePoint())
+    })
+    return `Sinh vien thap diem nhat la: ${list[0].name}`
+}
+
+function highestPointStudent(list){
+    list.sort((a,b) => {
+        return (b.avaragePoint() - a.avaragePoint())
+    })
+    return `Sinh vien cao diem nhat la: ${list[0].name}`
+}
+
+function printList(list){ //print the list of student
+    console.log("Danh sach sinh vien:")
+    for (let e of list){
+        console.log(e)
+    }
+}
